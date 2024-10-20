88a4fb3eed3a81155e4e5f64687bcc5eb8967d6c
biyk <biyk@inbox.ru>
2024-10-20 12:33:29 +0400

    bunker 3

--- bunker/script.js
+++ bunker/script.js
@@ -18,12 +18,13 @@ const myArray = [
     "https://player.vimeo.com/video/1010548635?h=7405a4c690&title=0&byline=0&portrait=0",
     "https://player.vimeo.com/video/1010548675?h=4dd04511f1&title=0&byline=0&portrait=0",
     "https://player.vimeo.com/video/1010548710?h=b5bd2c48dc&title=0&byline=0&portrait=0",
+	"14",
     "https://player.vimeo.com/video/1010548595?h=f81b31d151&title=0&byline=0&portrait=0",
     "https://player.vimeo.com/video/1014382431?h=736b720522&title=0&byline=0&portrait=0",
     "https://player.vimeo.com/video/1014382491?h=f476bf1dc1&title=0&byline=0&portrait=0",
     "https://player.vimeo.com/video/1014382554?h=e4e6a8528f&title=0&byline=0&portrait=0",
+	"19",
     "https://player.vimeo.com/video/1014382635?h=003b3595b8&title=0&byline=0&portrait=0",
-    "https://player.vimeo.com/video/1021229828?&title=0&byline=0&portrait=0",
     "https://player.vimeo.com/video/1019882063?h=dc5830d4b1&title=0&byline=0&portrait=0",
     "https://player.vimeo.com/video/1021078822?h=ce665ba11c&title=0&byline=0&portrait=0",
     "https://player.vimeo.com/video/1021079135?h=233477b0cf&title=0&byline=0&portrait=0",
@@ -42,12 +43,13 @@ function setBynumber(randomNum)
     audio.src = audioUrl;
 	iframe.src = iframeUrl;
 	
-	    audio.play();
+	if (!iframeUrl || iframeUrl.length==0) audio.play();
 
     startButton.style.display = 'none';
     image.style.display = 'block';
     audio.style.display = 'block';
     backButton.style.display = 'block';
+    iframe.style.display = 'block';
 }
 
 startButton.addEventListener('click', () => {

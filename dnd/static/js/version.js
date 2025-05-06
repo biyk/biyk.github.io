48112b65b77bda7b7e5b469d9df2de1b24680c6f
biyk <biyk@inbox.ru>
2025-05-06 21:11:51 +0400

    TODO продвигается

--- dnd/static/js/version.js
+++ dnd/static/js/version.js
@@ -1,1 +1,1 @@
-window.version = '0.2.37'
+window.version = '0.2.38'
--- todo/src/components/TodoList.vue
+++ todo/src/components/TodoList.vue
@@ -1,16 +1,17 @@
 <template>
-  <ul class="tasks">
-    <li
-      v-for="todo in todos"
-      :key="todo.id"
-      :class="{ completed: todo.completed }"
-      class="task"
-      @click="toggleTodo(todo.id)"
-    >
-      {{ todo.task }}
-      <span class="delete" @click="deleteTodo(todo.id)">ⓧ</span>
-    </li>
-  </ul>
+    <ul class="tasks">
+        <li
+            v-for="todo in filteredTodos"
+            :key="todo.id"
+            :class="{ completed: todo.completed }"
+            class="task"
+            @click="toggleTodo(todo.id)"
+
+        >
+            <span :title="todo.task_description">({{ todo.money_reward }}) {{ todo.task_title }}</span>
+            <span class="delete" @click="deleteTodo(todo.id)">ⓧ</span>
+        </li>
+    </ul>
 </template>
 
 <script>
@@ -19,6 +20,37 @@ export default {
     computed: {
         todos() {
             return this.$store.getters["todos/getTodos"];
+        },
+        filteredTodos() {
+            const now = new Date();
+            const today = now.getTime();
+            console.log(this.filter);
+            const tomorrow = (new Date(
+                now.getFullYear(),
+                now.getMonth(),
+                now.getDate() + 1, // Завтра
+                23, 59, 0, 0       // Время: 23:59:00.000
+            )).getTime();
+
+            return this.todos.filter(todo => {
+                if (todo.task_title === 'task_title') return false;
+
+                const start = todo.start_date; // предполагаем, что есть поле `due_date`
+                switch (this.filter) {
+                    case 'today':
+                        return start < today;
+                    case 'tomorrow':
+                        return start > today && start < tomorrow;
+                    default:
+                        return true;
+                }
+            });
+        }
+    },
+    props: {
+        filter: {
+            type: String,
+            default: 'all'
         }
     },
     methods: {

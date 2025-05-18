0f711bb4c92642eb60d9d2ff98f3b6d551be9d16
biyk <biyk@inbox.ru>
2025-05-18 18:09:29 +0400

    fix date

--- dnd/static/js/version.js
+++ dnd/static/js/version.js
@@ -1,1 +1,1 @@
-window.version = '0.2.75'
+window.version = '0.2.76'
--- todo/assets/index-03b86381.js
+++ todo/assets/index-03b86381.js
@@ -39,7 +39,7 @@
     fetch(link.href, fetchOpts);
   }
 })();
-window.version = "0.2.75";
+window.version = "0.2.76";
 /**
 * @vue/shared v3.5.13
 * (c) 2018-present Yuxi (Evan) You and Vue contributors

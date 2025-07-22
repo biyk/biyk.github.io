9a72794da5b0522d26508e4fd5bd2a52d679ee83
biyk <biyk@inbox.ru>
2025-07-22 22:14:26 +0400

    добавление задачи

--- todo/src/components/TodoNew.vue
+++ todo/src/components/TodoNew.vue
@@ -13,6 +13,7 @@
 <script>
 import '../assets/styles/components/TodoNew.css';
 import { generateUUIDv4 } from '@/utils/uuid';
+import {GoogleSheetDB, Table} from "../../../dnd/static/js/db/google.js";
 export default {
     data() {
         return {
@@ -21,11 +22,36 @@ export default {
         };
     },
     methods: {
-        addTodo: function() {
-            this.$store.dispatch("todos/addTodo", {
-                task_uuid: this.newId,
-                task_title: this.task_title
+        addTodo: async function () {
+            const api = window.GoogleSheetDB || new GoogleSheetDB();
+            await api.waitGoogle();
+
+            const settings = this.$store.getters["settings/allSettings"];
+            const spreadsheetSetting = settings.find(s => s.code === "spreadsheetId");
+
+            let table = new Table({
+                spreadsheetId: spreadsheetSetting.value,
+                list: "real_life_tasks"
             });
+
+            this.task_uuid = generateUUIDv4();
+            await table.addRow({
+                task_uuid: this.task_uuid,
+                task_title: this.task_title,
+                task_date: new Date().getTime(),
+                task_sort: 100,
+                task_time: 15,
+                repeat_index: 1,
+                date_mode: 1,
+                break_multiplier: 1,
+                task_color: 'blue',
+                money_reward: 10,
+                repeat_mode: 6,
+                task_finish_date: 0,
+                start_date: 0,
+                number_of_executions: 0
+            })
+
             this.task_uuid = generateUUIDv4();
             this.task_title = "";
         }

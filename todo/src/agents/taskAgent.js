1b0d390184b273cc111ce712ca087ee85004bd9f
biyk <biyk@inbox.ru>
2025-08-27 12:50:42 +0400

    fix deleted

--- todo/src/agents/taskAgent.js
+++ todo/src/agents/taskAgent.js
@@ -1,15 +1,20 @@
 import {listEvents} from "@/utils/calendar.js";
+import {GoogleSheetDB} from "../../../dnd/static/js/db/google.js";
 
 let intervalId = null;
 
-export function startTaskAgent(store) {
+export async function startTaskAgent(store) {
     if (intervalId) return; // агент уже работает
+    const api = window.GoogleSheetDB || new GoogleSheetDB();
 
     intervalId = setInterval(async () => {
-        store.dispatch("todos/initTodos");
-        const todos = store.getters['todos/getTodos'];
-        await listEvents(store);
-        const now = new Date();
+        if (!api.expired()){
+            store.dispatch("todos/initTodos");
+            const todos = store.getters['todos/getTodos'];
+            await listEvents(store);
+        } else {
+            console.warn('need auth')
+        }
 
     }, 60 * 1000); // раз в минуту
 }

824bb332f3995f67b174e66a3bfdf07b497832b3
biyk <biyk@inbox.ru>
2026-07-22 23:59:39 +0400

    fix styles

--- todo/assets/index-8600c5b6.js
+++ todo/assets/index-8600c5b6.js
@@ -39,7 +39,7 @@
     fetch(link.href, fetchOpts);
   }
 })();
-window.version = "0.5.37";
+window.version = "0.5.38";
 /**
 * @vue/shared v3.5.13
 * (c) 2018-present Yuxi (Evan) You and Vue contributors
